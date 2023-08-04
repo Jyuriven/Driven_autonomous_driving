6e6905a052f4518784019057dee136c4565574c6
proggg <xowjdpk@gmail.com>
2023-08-05 04:27:05 +0900

    feat : PATH SMOOTHING 과 MOTION PLANNING
    MOTION PLANNING 은 개즂만큼 했습니다.

--- decision/library/mapping_.py
+++ decision/library/mapping_.py
@@ -2,7 +2,6 @@
 Ref. A-star planning
 https://github.com/TThibeau/Lidar-Mapping-A-star-Path-Planning/blob/main/code/mapping_functions.py
 '''
-
 import numpy as np
 import PIL as Image
 
@@ -15,31 +14,31 @@ def grid_to_img(img_grid,name,rgb_tuple):
     pil_image.save(f'Out\{name}.png')
     # pil_image.show()
 
-def measurement_to_point_cloud(angle_dist,x_veh,z_veh,veh_heading)->list[(float,float)]:
-    '''
-    This function converts the measurements of a 2D (horizontal) Lidar in which each measurement consists of a list of tuples (angle,distance)
-    and returns a point cloud (list of tuples with x,z coordinates)
+# def measurement_to_point_cloud(angle_dist,x_veh,z_veh,veh_heading)->list[(float,float)]:
+#     '''
+#     This function converts the measurements of a 2D (horizontal) Lidar in which each measurement consists of a list of tuples (angle,distance)
+#     and returns a point cloud (list of tuples with x,z coordinates)
 
-    Input:  angle_dist = [(a_1,d_1),(a_2,d_2),...,(a_n,d_n)]
-            x_veh = x-coordinate of vehicle
-            z_veh = z-coordinate of vehicle
-            veh_heading = heading of the vehicle in degrees (0 deg=N, 90 deg=E, 180 deg=S, 270 deg=W)
+#     Input:  angle_dist = [(a_1,d_1),(a_2,d_2),...,(a_n,d_n)]
+#             x_veh = x-coordinate of vehicle
+#             z_veh = z-coordinate of vehicle
+#             veh_heading = heading of the vehicle in degrees (0 deg=N, 90 deg=E, 180 deg=S, 270 deg=W)
 
-    Output: [(x1,z1),(x2,z2),...,(xn,zn)]
-    '''
-    point_cloud = []
+#     Output: [(x1,z1),(x2,z2),...,(xn,zn)]
+#     '''
+#     point_cloud = []
 
-    for i in range(len(angle_dist)):
-        angle = np.deg2rad(angle_dist[i][0])+np.deg2rad(veh_heading)
+#     for i in range(len(angle_dist)):
+#         angle = np.deg2rad(angle_dist[i][0])+np.deg2rad(veh_heading)
 
-        dist = angle_dist[i][1]
+#         dist = angle_dist[i][1]
 
-        x = round((dist * np.sin(angle) + x_veh),2) # Rounded x-coordinate with 2 decimals
-        z = round((dist * np.cos(angle) + z_veh),2) # Rounded z-coordinate with 2 decimals
+#         x = round((dist * np.sin(angle) + x_veh),2) # Rounded x-coordinate with 2 decimals
+#         z = round((dist * np.cos(angle) + z_veh),2) # Rounded z-coordinate with 2 decimals
 
-        point_cloud.append((x,z))
+#         point_cloud.append((x,z))
     
-    return point_cloud
+#     return point_cloud
 
 
 
@@ -52,10 +51,10 @@ def init_root_grid(height,width,resolution):
     if COLS % 2 == 0: 
         COLS += 1 # The grid should have odd sizes (to have a mid col)
     
-    main_grid = np.zeros((ROWS,COLS,3))
+    main_grid = np.zeros((ROWS,COLS,3), dtype=np.float32)
+    
 
     return main_grid
-    return 
 
 def point_cloud_to_grid(pc,grid,res):
     '''

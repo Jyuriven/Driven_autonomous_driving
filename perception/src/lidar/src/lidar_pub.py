7d71f300202b51e9370a84ba6c1537944fa05c14
yyongjae <dydwo706@naver.com>
2023-08-13 15:58:40 +0900

    feat: slam 실행까지 가능한 코드 수정 및 폴더 구조 재구성

--- perception/src/lidar/src/lidar_pub.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from sensor_msgs.msg import PointCloud2
-import sensor_msgs.point_cloud2 as pc2
-
-def lidar_publisher():
-    
-
-if __name__ == '__main__':
--- perception/src/slam/include/utility.h
+++ perception/src/slam/include/utility.h
@@ -10,7 +10,7 @@
 
 #include "cloud_msgs/cloud_info.h"
 
-#include <opencv/cv.h>
+#include <opencv2/core.hpp>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>

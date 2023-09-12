2417044700ef36b9999fec5cc4842393b6784fcb
yyongjae <dydwo706@naver.com>
2023-09-12 21:47:57 +0900

    exp: python파일 패키지에 추가

---
+++ perception/slam/LeGO_LOAM/scripts/mapConvert.py
@@ -0,0 +1,19 @@
+import rospy
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+import numpy as np
+
+def cloud_callback(msg):
+    pc_data = pc2.read_points(msg, field_names=("x", "y", "z"), skip_nans=True)
+    point_cloud = np.array([p[:3] for p in pc_data])
+
+    print(f'array:::::{point_cloud}')
+
+def main():
+    rospy.init_node('mapconvert', anonymous=True)
+    rospy.Subscriber("/segmented_cloud_pure", PointCloud2, cloud_callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()

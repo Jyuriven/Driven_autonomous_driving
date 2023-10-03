18b6f69571d765be5e58c34b185dd21960fd8c01
Yujaemin <71584316+jaeminyu2356@users.noreply.github.com>
2023-10-03 20:50:17 +0900

    Update main.py

--- control/main.py
+++ control/main.py
@@ -5,23 +5,23 @@ import time
 def main():
 	rospy.init_node('rosout') 	
 	master_control = Controller()
-	sample_angle=[0.0,3.0,5.0,16.0,-4.0]
-	sample_cur=[0.0,10.0,20.0,30.0,20.0]
-	sample_liner=[5.0,13.0,25.0,32.0,19.0]
+	###샘플 데이터###
+	#sample_angle=[0.0,3.0,5.0,16.0,-4.0]
+	#sample_cur=[0.0,10.0,20.0,30.0,20.0]
+	#sample_liner=[5.0,13.0,25.0,32.0,19.0]
 	# while True:
-	# 	Throttle, Brake, Steering = master_control.control(10, 10, -25.0, 0)
-	# 	print(Throttle,Brake,Steering)
-	# 	time.sleep(1)
-	# now = time.time()
-	# time.sleep(3)
-	# next_time = time.time()
-	# print(now - next.time)
-	for i in range(5):
-		# while(now - next_time):
-		print("11111",i,sample_cur[i],sample_liner[i],sample_angle[i])
-		Throttle, Brake, Steering = master_control.control(sample_cur[i],sample_liner[i],sample_angle[i],0)
-		next_time = time.time()
-		# now = time.time()
+	#Throttle, Brake, Steering = master_control.control(10, 10, -25.0, 0)
+	#print(Throttle,Brake,Steering)
+	
+	### currnet_vel -> 현재 속도
+	### linear_vel -> 목표 속도
+	### angular -> 목표 헤딩각
+	### brake -> 브레이크 값
+	### 브레이크 값 연산은 control
+	### 파트에서 이루어지지 않고 
+	### motion planning에서 연산 및 목표한
+	### 데이터를 그대로 보냄
+	Throttle, Brake, Steering = master_control.control(current_vel,linear_vel,angular,brake)
         
 if __name__ == "__main__":
     main()
